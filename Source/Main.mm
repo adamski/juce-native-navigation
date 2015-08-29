28c6d26552e5243d1b0dd00af87bbeec1f9350d0
Adam Wilson <adam@codegarden.co.uk>
2015-08-29 10:20:29 +0100

    tidy up deadcode

--- Source/Main.mm
+++ Source/Main.mm
@@ -15,38 +15,6 @@
 #import "TestViewController.h"
 #import "MainViewController.h"
 
-@interface ViewLoader : NSObject 
-{
-}
-
-@property UINavigationController *navController;
-
-- (void) nextView: (id) sender;
-
-@end
-
-
-
-@implementation ViewLoader
-
-- (id) initWithNavigationController: (UINavigationController *) navController {
-    self = [super init];
-    
-    if (self) {
-        _navController = navController;
-    }
-    
-    return self;
-}
-
-
-- (void) nextView: (id) sender {
-    
-    TestViewController *testViewController = [[TestViewController alloc] initWithNibName:@"TestViewController" bundle:nil];
-    [_navController pushViewController:testViewController animated:YES];
-}
-
-@end
 
 
 //==============================================================================
@@ -127,158 +95,14 @@ public:
             [viewController addContentView];
             
             
-            
-
-            
-            
-            //viewController.contentView = juceView;
-            
-            //TestViewController *testViewController = [[TestViewController alloc] initWithNibName:@"TestViewController" bundle:nil];
-            
-            
             navController = [[UINavigationController alloc] initWithRootViewController: viewController];
             [iosWindow setRootViewController:navController];
-            
-            //------------------------------------------------------
-            
             
             
             //[iosWindow addSubview: [viewController view]];
             [iosWindow makeKeyAndVisible];
-            
-            /*
-            navBar = [[UINavigationBar alloc] initWithFrame:CGRectMake(0, 0, CGRectGetWidth(juceView.bounds), 32.0)];
-            [navBar setAutoresizingMask:UIViewAutoresizingFlexibleWidth];
-            [navBar setBarTintColor:[UIColor blackColor]];
-            [navBar setTintColor:[UIColor grayColor]];
-            
-            NSDictionary *navbarTitleTextAttributes = [NSDictionary dictionaryWithObjectsAndKeys:
-                                                                                                                      [UIColor whiteColor],NSForegroundColorAttributeName, nil]; //,
-                                                       //                                                               //[UIColor grayColor], UITextAttributeTextShadowColor,
-                                                       //                                                               //[NSValue valueWithUIOffset:UIOffsetMake(-1, 0)], UITextAttributeTextShadowOffset, nil];
-                                                       //                    
-            [[UINavigationBar appearance] setTitleTextAttributes:navbarTitleTextAttributes];
-            
-            UINavigationItem *navItem = [[UINavigationItem alloc] init];
-            navItem.title = @"Test";
-            [navBar setItems:@[navItem]];
-            
-            ViewLoader *viewLoader = [[ViewLoader alloc] initWithNavigationController: navController];
-            //
-            UIBarButtonItem *rightButton = [[UIBarButtonItem alloc] initWithTitle:@"Edit"
-                                                                            style:UIBarButtonItemStylePlain
-                                                                            target:viewLoader
-                                                                            action:@selector(nextView:)];
-            navItem.rightBarButtonItem = rightButton;
-            
-            //navController
-            //[juceView addSubview:navBar];
-            */
             #endif
             
-//            //UIView* view = (UIView*) getTopLevelComponent()->getWindowHandle();
-//            //UIViewController* viewController = view.rootViewController;
-//            
-//            
-//            UIView* view = (UIView*) getTopLevelComponent()->getWindowHandle();
-//            UIResponder* viewResponder = view.nextResponder;
-//            
-//            if ([viewResponder isKindOfClass: [UIViewController class]])
-//            {
-//                UIViewController* controller = (UIViewController*) viewResponder;
-//                //xxx
-//                
-//                //self.viewController = [[YourViewControllername alloc] initWithNibName:@"YourViewControllername" bundle:nil]
-//                
-//
-//                UIResponder* controllerResponder = ((UIWindow*) controller.nextResponder);
-//                
-//                DBG ("Got UIViewController");
-//                
-//                if ([controllerResponder isKindOfClass: [UIWindow class]])
-//                {
-//                    UIWindow* window = (UIWindow*) controllerResponder;
-//                    
-//                    window.rootViewController = nil; // <-- prevents error "adding a root view controller <JuceUIViewController> as a child of view controller <UINavigationController>"
-//                    navController = [[UINavigationController alloc] initWithRootViewController:controller];
-//                    
-//                    [window setRootViewController:navController];
-//                    [window addSubview:[navController view]];
-//                    [window makeKeyAndVisible];
-//                    
-//                    //-------------------------------------------------------------
-//                    
-////                    UIBarButtonItem * button = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone
-////                                                                                             target:controller
-////                                                                                             action:@selector(done:)];
-////                    [[controller navigationItem] setRightBarButtonItem:button];
-////                    [button release];
-//                    
-//                    //CGRect frame = view.bounds;
-//                    
-//                    //navBar = [[UINavigationBar alloc] initWithFrame:frame];
-//                    navBar = [[UINavigationBar alloc] initWithFrame:CGRectMake(0, 0, CGRectGetWidth(view.bounds), 32.0)];
-//                    //frame.size = [navBar sizeThatFits:frame.size];
-//                    //[navBar setFrame:frame];
-//                    [navBar setAutoresizingMask:UIViewAutoresizingFlexibleWidth];
-//                    //[navBar setItems:[NSArray arrayWithObject:controller.navigationItem]];
-//                    
-//                    //navBar.barTintColor = [UIColor blueColor];
-//                    //navController.navigationBar.tintColor = [UIColor blueColor];
-//                    [navBar setBarTintColor:[UIColor blackColor]];
-//                    [navBar setTintColor:[UIColor grayColor]];
-//                    
-//                    
-//                    // 1. hide the existing nav bar
-//                    //[navController setNavigationBarHidden:YES animated:NO];
-//                    
-//                    // 2. create a new nav bar and style it
-////                    UINavigationBar *newNavBar = [[UINavigationBar alloc] initWithFrame:CGRectMake(0, 0, CGRectGetWidth(view.bounds), 32.0)];
-////                    [newNavBar setBarTintColor:[UIColor blackColor]];
-////                    [newNavBar setTintColor:[UIColor grayColor]];
-//                    
-//                    NSDictionary *navbarTitleTextAttributes = [NSDictionary dictionaryWithObjectsAndKeys:
-//                                                               [UIColor whiteColor],NSForegroundColorAttributeName, nil]; //,
-//                                                               //[UIColor grayColor], UITextAttributeTextShadowColor,
-//                                                               //[NSValue valueWithUIOffset:UIOffsetMake(-1, 0)], UITextAttributeTextShadowOffset, nil];
-//                    
-//                    [[UINavigationBar appearance] setTitleTextAttributes:navbarTitleTextAttributes];
-//                    
-//                    // 3. add a new navigation item w/title to the new nav bar
-//                    UINavigationItem *navItem = [[UINavigationItem alloc] init];
-//                    navItem.title = @"Test";
-//                    [navBar setItems:@[navItem]];
-//                    
-//                    // Add button
-//                    ViewLoader *viewLoader = [[ViewLoader alloc] initWithNavigationController: navController];
-//                    
-//                    UIBarButtonItem *rightButton = [[UIBarButtonItem alloc] initWithTitle:@"Edit"
-//                                                                                   style:UIBarButtonItemStylePlain
-//                                                                                   target:viewLoader
-//                                                                                   action:@selector(nextView:)];
-//                    navItem.rightBarButtonItem = rightButton;
-//                    
-//                    
-//                    // 4. add the nav bar to the main view
-//                    [view addSubview:navBar];
-//                    
-//                    // UIBarButtonItem *sendButton = [[UIBarButtonItem alloc]
-//                    //                                initWithTitle:@"Send"
-//                    //                                style:UIBarButtonItemStylePlain
-//                    //                                target:navController
-//                    //                                action:@selector(sendEmail)];
-//                    //
-//                    // navController.navigationItem.rightBarButtonItem = sendButton;
-//                    //
-//                    //[view addSubview:navBar];
-//                    //[controller.view addSubview:navBar];
-//                    
-//                    DBG ("Got UIWindow");
-//                    
-//                }
-//
-//            }
-
             
         }
 
