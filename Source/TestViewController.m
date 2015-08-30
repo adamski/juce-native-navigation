95e91fce1176a8a321521203e66c981a89f3beee
Adam Wilson <adam@codegarden.co.uk>
2015-08-30 02:50:05 +0100

    add new source files

---
+++ Source/TestViewController.m
@@ -0,0 +1,38 @@
+//
+//  TestViewController.m
+//  iosNavigation
+//
+//  Created by Adam Elemental on 27/08/2015.
+//
+//
+
+#import "TestViewController.h"
+
+@interface TestViewController ()
+
+@end
+
+@implementation TestViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+    [[self navigationItem] setTitle: @"World"];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
